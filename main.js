1acfa3808be1195e0676bb2a5a374a18377c6834
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-08 17:57:15 +0100

    className

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 
-// primo metodo
-
 const element = document.querySelector('.header-right > a');
 const hamburgher = document.querySelector('.hamburger-menu');
 const menucloser = document.querySelector('header .hamburger-menu .close');
 
-element.addEventListener("click", 
-function() {
-  element.remove()
-  hamburgher.style.display = 'block';
-});
+// primo metodo
 
-menucloser.addEventListener("click", 
-function() {
-  hamburgher.style.display = 'none';
-});
+// element.addEventListener("click", 
+// function() {
+//   hamburgher.style.display = 'block';
+// });
+
+// menucloser.addEventListener("click", 
+// function() {
+//   hamburgher.style.display = 'none';
+// });
 
 // secondo metodo
+element.addEventListener("click", 
+function() {
+  document.querySelector(".header-right > a").className = ".hamburgher-menu";
+});
+
+
+
+
+
+
 
 
 
